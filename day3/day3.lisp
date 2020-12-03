5df394a9703ebfbe5fedabe903ac1f296c7cef33
sergio garcia <sergio.garcia@gmail.com>
2020-12-03 09:15:32 -0500

    day3

--- day3/day3.lisp
+++ day3/day3.lisp
@@ -14,5 +14,19 @@
 (defun get-map-val (row col)
 	(aref (aref *input* row) (mod col (length (aref *input* 0)))))
 
+(defun tree-p (row col)
+	(eq #\# (get-map-val row col)))
+
+(defun count-trees (right down)
+	(loop for row from down upto (1- (length *input*)) by down
+				for col from right by right
+				counting (tree-p row col)))
+
+;; part 1
+(count-trees 3 1)
+
+;; part 2
+(apply #'* (loop for (right down) in '((1 1) (3 1) (5 1) (7 1) (1 2))
+								 collecting (count-trees right down)))
 
 (read-input "input")
